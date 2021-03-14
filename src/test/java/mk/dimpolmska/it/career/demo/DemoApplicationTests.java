8324344999c0c00aee86cd5e7f004bb6be004902
ArifeLjutviovska <arife.lyutvioska@hotmail.com>
2021-03-14 15:55:07 +0100

    This is a new commit for what I originally planned to be amended

---
+++ src/test/java/mk/dimpolmska/it/career/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.dimpolmska.it.career.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
